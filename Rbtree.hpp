fd0fa0edda191462ad12850396be4033e95aebf2
Rodolpho Cammarosano <rcammaro@student.s19>
2021-10-15 18:00:51 +0200

    up

--- Rbtree.hpp
+++ Rbtree.hpp
@@ -42,35 +42,55 @@ struct node
 class Rbtree
 {
 private:
+	typedef	std::size_t size_type;
+
 	CompValue		_comp;
 	node 			_end;	// stack variable
 	node *			&_root;	// reference to _end.left
 	node			_nil;
-	unsigned long	_size;
+	size_type		_size;
 
 	void	update_end();
 	bool	insert(node* &root, node* parent, int const &value);
 	void	insert_fix(node *inserted_node);
 	node *	new_node(int const &value);
-	void	delete_node(node* &x);
-	void	clear(node* &root);
+	void	clear_node(node* &x);
+	void	clear_tree(node* &root);
 	void	print(node* root, int space) const;
 	bool	is_left_child(node *x) const;
 	bool	is_red(node *x) const;
+	bool	is_black(node *x) const;
 	void	left_rotate(node * x);
 	void	right_rotate(node * x);
 	node*	find(node *root, int const &value);
+	void	remove_node(node *target);
+	void	transplant(node *old, node *new_node);
+	void	detach_nil_node();
+	node *	successor(node *x) const;
+
+	// fix_remove
+	void	fix_remove(node * deleted, node * replacement, node * x);
+	node *	sibling(node *x);
+	int		case_index(node *x);
+	void	run_case(int index, node * x);
+	void	case_1(node *x);
+	void	case_2(node *x);
+	void	case_3(node *x);
+	void	case_4(node *x);
+
 
 public:
 	Rbtree();
 	~Rbtree();
 
-	void	insert(int const &value); 
-	void 	clear();
-	void 	print() const;
-	node*	find(int const &value);
+	void		insert(int const &value); 
+	size_type	erase(int const &value);
+	void 		clear();
+	void 		print() const;
+	node*		find(int const &value);
 
 	node const *	getEnd() const; // debug only
+	size_type		size() const;
 };
 
 
@@ -95,11 +115,17 @@ public:
 
 Rbtree::Rbtree():_end(0), _root(_end.left), _nil(0), _size(0)
 {
+	_nil.color = rbt_black;
 }
 
 Rbtree::~Rbtree()
 {
 	clear();
+}
+
+Rbtree::size_type	Rbtree::size() const
+{
+	return (_size);
 }
 
 // whenever the root changes, _end.left holds its address, but _end.righ
@@ -118,6 +144,7 @@ bool Rbtree::insert(node* &root, node* parent, int const &value)
 {
 	if (!root)
 	{
+		std::cout << "inserting " << value << std::endl;
 		root = new_node(value);
 		root->parent = parent;
 		insert_fix(root);
@@ -213,6 +240,12 @@ bool	Rbtree::is_red(node *x) const
 	return false;
 }
 
+// x can be null
+bool	Rbtree::is_black(node *x) const
+{
+	return (!is_red(x));
+}
+
 // x->right cannot be NULL
 void Rbtree::left_rotate(node * x)
 {
@@ -287,6 +320,7 @@ void Rbtree::print(node *root, int space) const
 void Rbtree::print() const
 {
 	print(_root, 0);
+	std::cout << "---------------------------\n";
 }
 
 node * Rbtree::new_node(int const &value)
@@ -296,26 +330,26 @@ node * Rbtree::new_node(int const &value)
 	return (new node(content));
 }
 
-void Rbtree::delete_node(node* &x)
+void Rbtree::clear_node(node* &x)
 {
 	delete x->value;
 	delete x;
 	x = NULL;
 }
 
-void Rbtree::clear(node* &root)
+void Rbtree::clear_tree(node* &root)
 {
 	if (root)
 	{
-		clear(root->left);
-		clear(root->right);
-		delete_node(root);
+		clear_tree(root->left);
+		clear_tree(root->right);
+		clear_node(root);
 	}
 }
 
 void Rbtree::clear()
 {
-	clear(_root);
+	clear_tree(_root);
 	update_end();
 	_size = 0;
 }
@@ -342,6 +376,264 @@ node* Rbtree::find(int const &value)
 node const * Rbtree::getEnd() const
 {
 	return (&_end);
+}
+
+
+Rbtree::size_type	Rbtree::erase(int const &value)
+{
+	node *target = find(value);
+	if (target == &_end)
+		return (0);
+
+	std::cout << "deleting " << value << std::endl;
+	remove_node(target);
+	return (1);
+}
+
+void	Rbtree::transplant(node *old, node *new_node)
+{
+	// update parent
+	if (old == old->parent->left) // old is a left child
+		old->parent->left = new_node;
+	else
+		old->parent->right = new_node;
+	// set new_node's pointer to parent
+	new_node->parent = old->parent;
+}
+
+void	Rbtree::remove_node(node *target)
+{
+	node * replacement;
+	node * x;
+
+	if (target->left && target->right) // node has two children
+	{
+		replacement = successor(target);
+		x = replacement->right;
+		if (!x)
+			x = &_nil;
+		transplant(replacement, x); // detach replacement from tree
+		transplant(target, replacement); // replace target
+		replacement->left = target->left;
+		replacement->left->parent = replacement;
+		replacement->right = target->right;
+		replacement->right->parent = replacement;
+	}
+	else 
+	{
+		if (target->left) // left child only
+			replacement = x = target->left;
+		else if (target->right) // right child only
+			replacement = x = target->right;
+		else	// no children
+			replacement = x = &_nil;
+		transplant(target, replacement);
+	}
+	fix_remove(target, replacement, x);
+	detach_nil_node();
+	clear_node(target); 
+	--_size;
+	update_end();
+}
+
+void	Rbtree::detach_nil_node()
+{
+	if (_nil.parent)
+	{
+		if (&_nil == _nil.parent->left)
+			_nil.parent->left = NULL;
+		else
+			_nil.parent->right = NULL;
+		_nil.parent = NULL;
+	}
+}
+
+// x->right cannot be NULL
+node *	Rbtree::successor(node *x) const
+{
+	x = x->right;
+
+	while (x->left)
+		x = x->left;
+	return (x);
+}
+
+void Rbtree::fix_remove(node * deleted, node * replacement, node * x)
+{
+	if (deleted->color == rbt_red)
+	{
+		if (replacement->color == rbt_red || replacement == &_nil) // first case
+		{
+			// std::cout << "initial step # 1 \n";
+			return ;
+		}
+		// replacement is black - second case
+		replacement->color = rbt_red;
+		// std::cout << "initial step # 2 \n";
+		run_case(case_index(x), x);
+	}
+	else // delete node is black
+	{
+		if (is_red(replacement)) // third case
+		{
+			// std::cout << "initial step # 3 \n";
+			replacement->color = rbt_black;
+		}
+		else if (x != _root) // fourth case
+		{
+			// std::cout << "initial step # 4 \n";
+			run_case(case_index(x), x);
+		}
+		// fifth case: x is the root, we are done.
+		// else
+			// std::cout << "initial step # 5 \n";
+	}
+}
+
+node * Rbtree::sibling(node *x)
+{
+	if (x == _root) // debug only
+	{
+		std::cout << "sibling of root attempt ... " << std::endl;
+		return (NULL);
+	}
+	if (is_left_child(x))
+		return (x->parent->right);
+	return (x->parent->left);
+}
+
+int Rbtree::case_index(node *x)
+{
+	if (x->color == rbt_red)
+		return (0);
+	// x is black
+	node * w = sibling(x);
+	if (!w) // debug
+	{
+		std::cout << "sibling is NULL!" << std::endl;
+		return (-1);
+	}
+	if (w->color == rbt_red)
+		return (1);
+	// w is black
+	if (is_black(w->left) && is_black(w->right))
+		return (2);
+	// at lest one of w's children is red
+	if (is_left_child(x))
+	{
+		if (is_black(w->right))
+			return (3);
+		return (4);
+	}
+	else // x is right_child
+	{
+		if (is_black(w->left))
+			return (3);
+		return (4);
+	}
+}
+
+void Rbtree::run_case(int index, node * x)
+{
+	if (index < 0 || index > 4) // debug
+	{
+		std::cout << "bad index" << std::endl;
+		return ;
+	}
+	// std::cout << "case " << index << "\n";
+
+	if (index == 0)
+		x->color = rbt_black;
+	else if (index == 1)
+		case_1(x);
+	else if (index == 2)
+		case_2(x);
+	else if (index == 3)
+		case_3(x);
+	else
+		case_4(x);
+}
+
+// 	Case 1: Node x is black and its sibling w is red
+void Rbtree::case_1(node *x)
+{
+	node * w = sibling(x);
+
+	// 1. Color w black
+	w->color = rbt_black;
+
+	// 2. Color x.p red
+	x->parent->color = rbt_red;
+
+	// 3. Rotate x.p
+	// a. If x is the left child do a left rotation
+	if (is_left_child(x))
+		left_rotate(x->parent);
+	// b. If x is the right child do a right rotation
+	else
+		right_rotate(x->parent);
+
+	// 4. Now we have to change w
+	// a. If x is the left child set w = x.p.right
+	// b. If x is the right child set w = x.p.left
+	// 5. With x and our new w, decide on case 2, 3, or 4 from here.
+	int case_i = case_index(x);
+	if (case_i < 2)
+	{
+		std::cout << "case index should be 2, 3 or 4" << std::endl;
+		return ;
+	}
+	run_case(case_i, x);
+}
+
+// Node x is black and its sibling w is black and both of w's children
+// are black
+void Rbtree::case_2(node *x)
+{
+	node * w = sibling(x);
+	w->color = rbt_red;
+	x = x->parent;
+	if (is_red(x))
+		x->color = rbt_black;
+	else if (x != _root)
+		run_case(case_index(x), x);
+}
+
+void Rbtree::case_3(node *x)
+{
+	node * w = sibling(x);
+
+	w->color = rbt_red;
+
+	if (is_left_child(x))
+	{
+		w->left->color = rbt_black;
+		right_rotate(w);
+	}
+	else
+	{
+		w->right->color = rbt_black;
+		left_rotate(w);
+	}
+	run_case(4, x);
+}
+
+void Rbtree::case_4(node *x)
+{
+	node * w = sibling(x);
+	
+	w->color = x->parent->color;
+	x->parent->color = rbt_black;
+	if (is_left_child(x))
+	{
+		w->right->color = rbt_black;
+		left_rotate(x->parent);
+	}
+	else
+	{
+		w->left->color = rbt_black;
+		right_rotate(x->parent);
+	}
 }
 
 #endif
--- unit_test_Rbtree.cpp
+++ unit_test_Rbtree.cpp
@@ -1,10 +1,13 @@
 #include <criterion/criterion.h>
 #include "Rbtree.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 Rbtree t;
 
 // int main()
-Test(tree, test)
+void setup()
 {
 	t.insert(5);
 	t.insert(10);
@@ -18,10 +21,92 @@ Test(tree, test)
 	t.insert(24);
 	t.insert(26);
 	t.insert(28);
-	// t.print();
+}
 
+Test(find, test, .init=setup)
+{
 	cr_expect(t.find(5) != t.getEnd());
 	cr_expect(t.find(3) != t.getEnd());
 	cr_expect(t.find(50) == t.getEnd());
+
+}
+
+Test(remove, leaf_node, .init=setup)
+{
+
+	cr_expect(t.erase(28));
+	cr_expect(!t.erase(50));
+}
+
+Test(remove, one_child_node, .init=setup)
+{
+	cr_expect(t.erase(26));
+	cr_expect(t.erase(5));
+}
+
+Test(remove, two_children_node, .init=setup)
+{
+	cr_expect(t.erase(24));
+	cr_expect(t.erase(1));
+	cr_expect(t.erase(10)); // the root
+	cr_expect(t.find(28));
+}
+
+Test(remove, remove_fix, .init=setup)
+{
+	cr_expect(t.erase(28));
+	cr_expect(t.erase(26));
+	cr_expect(t.erase(11));
+	cr_expect(t.erase(10)); // root
+}
+
+void infix_add2vector(node *root, std::vector<int> &v)
+{
+	if (!root)
+		return ;
+	infix_add2vector(root->left, v);
+	v.push_back(*root->value);
+	infix_add2vector(root->right, v);
+}
+
+std::vector<int> btree2vector(Rbtree &t)
+{
+	std::vector<int> v;
+	node * root = ((t.getEnd())->left);
+	// node * root = const_cast<node *>((t.getEnd())->left);
+	infix_add2vector(root, v);
+	return (v);
+}
+
+
+Test(remove, random)
+{
+	Rbtree tree;
+	int const n = 1000;
+	int index;
+	int	new_nb;
+
+	std::srand(std::time(NULL));
+	for (int i = 0; i < n; i++)
+		tree.insert(std::rand() % n);
+	
+	std::vector<int> v = btree2vector(tree);
+	// for (size_t i = 0; i < v.size(); i++)
+	// {
+	// 	std::cout << v[i] << ' ';
+	// }
+	
+	while (v.size())
+	{
+		index = std::rand() % v.size();
+		tree.erase(v[index]);
+		v.erase(v.begin() + index);
+		if (index % 2)
+		{
+			new_nb = std::rand() % n;
+			tree.insert(new_nb);
+			v.push_back(new_nb);
+		}
+	}
 
 }
